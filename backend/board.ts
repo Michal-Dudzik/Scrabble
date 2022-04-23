16ac4166c3e6f9ee9ef8f7e036c01d2c5d55c973
DarkB0B <miecze123@onet.pl>
2022-04-24 00:10:27 +0200

    .

--- backend/board.ts
+++ backend/board.ts
@@ -1,24 +1,27 @@
-import { BoardTile } from "./boardtile"
+import { EmptyTile } from "./boardtile"
+import { LetterTile } from "./boardtile"
+import { ITile } from "./boardtile"
 export class Board
 {
     id:number 
-    gameboard:BoardTile[][] = []
+    gameboard :any[][] = []
     players:string[] //list of player id's that are currently playing 
     constructor(x:Board)
     {
         this.id = x.id
         this.players = x.players    
     }
-    public GenerateBoard() //method generating board
+    public GenerateEmptyBoard() //method generating board
     {
-        const emptyTile:BoardTile = {}
+        
         const rows:number = 14
         const columns:number = 14
         for (var i = 0; i < rows; i++)
         {
              for (var j = 0; j < columns; j++)
              {
-                this.gameboard[i][j].push();
+                const emptytile: EmptyTile = new EmptyTile()
+                this.gameboard[i][j].push(emptytile);
              }
         }       
     }
--- backend/boardtile.ts
+++ backend/boardtile.ts
@@ -1,13 +1,32 @@
-export class BoardTile
+export interface ITile
 {
-    id:number 
-    isempty:boolean //made to be able to check if you can assign letter to tile
-    tiletype:number //temporary; most likely type = enum [differently scored tiles]
-    letter:string //null or storing letter
-    constructor(x:BoardTile)
+    
+    readonly type: number // 0 = empty else its letter (A = 1, B = 2...)
+    readonly value: number 
+    readonly status: number// 0 = tile in storage / 1 = tile is in player's hand / 2 = tile is on gameboard during acceptance phase / 3 = tile is placed on board 
+}
+
+export class EmptyTile implements ITile
+{
+    
+    readonly type: number // 0 = empty tile
+    readonly value: number // 0 = empty tile
+    public status: number // 3 because empty tile can only apear on gameboard during board generation / 4 if tile contains bonus (probably wont be implemented)
+    public constructor(){
+
+    }
+    
+   
+}
+export class LetterTile implements ITile
+{
+    readonly type: number // A = 1, B = 2 etc
+    readonly value: number // value of tile that is used in counting score
+    public status: number 
+    public id: number // every letter tile has its unique id
+
+    public constructor() // TO DO
     {
-        this.isempty = x.isempty
-        this.tiletype = x.tiletype
-        this.letter = x.letter
+        
     }
 }
