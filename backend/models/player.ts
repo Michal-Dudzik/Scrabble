f0f69276558dc85d6c8452e665e22550824f8668
DarkB0B <miecze123@onet.pl>
2022-05-12 01:08:17 +0200

    SMOL changes

--- backend/models/player.ts
+++ backend/models/player.ts
@@ -1,5 +1,5 @@
 import { PlayerHand } from "./tilestorage";
-export class player
+export class Player
 {
     id:number; //value by which player can be recognized
     nickname: string; //can be set by player but doesnt serve any bigger reason
